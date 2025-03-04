c03be4d5bd27523e7ab31c97e1ff721b90596f9c
scox1502 <samuelc506@deltalearns.ca>
2025-03-04 19:27:39 +0000

    added the dagger into the game

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+gold = []
+invintory = []
 while True:
-    user_name = input("please enter your name. ").strip().lower()
+    user_name = input("please enter your username ").strip().lower()
     print(f"hello {user_name}")
 
-    print("please note this game has no checkpoints at this momant in time and is in early beta stages")
+    print("please note this game has no checkpoints at this momant in time so if you exit the game you will lose everything altho if you die you will respon close to where you died")
     choice = input("are you ready to start? [yes/no] ")
     if choice == "yes":
         break
@@ -12,8 +14,9 @@ while True:
         exit()
 
 while True:
-    choice = input("you wake up in a cave with your arms chaned to two torches what do you do [try and break the chanes/wait] ")
-    if choice == "try and break the chanes":
+    choice = input("you wake up in a cave with your arms chaned to two torches what do you do [break the chanes/wait] ")
+    if choice == "break the chanes":
+        print("you break out of the chanes with ease and notice a rusty small dagger to your left and you hear running footsteps on your right")
         break
 
     if choice == "wait":
@@ -25,4 +28,14 @@ while True:
     
     if choice == "no":
         exit()
+
+while True:
+    choice = input("do you take the rusty dagger? [yes/no] ")
+    if choice == "yes":
+        print("DAGGER HAS BEEN ADDED TO YOUR INVINTORY")
+        invintory.append("dagger")
+        break
+    if choice == "no":
+        print("you leave the dagger on the floor as your hear the people running get closer and closer")
+        break
 
